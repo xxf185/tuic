68917bdaef4ea989f5d2c81dfda88d0dc597a0a3
xxf185 <109097973+xxf185@users.noreply.github.com>
2023-12-31 14:17:12 +0800

    Update tuic.sh

--- tuic.sh
+++ tuic.sh
@@ -180,156 +180,7 @@ tuic_port(){
     done
     yellow "将在 tuic 节点使用的端口是：$port"
 }
-
-inst_tuv4(){
-    check_ip
-
-    if [[ ! ${SYSTEM} == "CentOS" ]]; then
-        ${PACKAGE_UPDATE}
-    fi
-    ${PACKAGE_INSTALL} wget curl sudo
-
-    wget https://gitlab.com/Misaka-blog/tuic-script/-/raw/main/files/tuic-latest-linux-$(archAffix) -O /usr/local/bin/tuic
-    if [[ -f "/usr/local/bin/tuic" ]]; then
-        chmod +x /usr/local/bin/tuic
-    else
-        red "Tuic V4 内核安装失败！"
-        exit 1
-    fi
-
-    tuic_cert
-    tuic_port
-
-    read -p "设置 tuic Token（回车跳过为随机字符）：" token
-    [[ -z $token ]] && token=$(date +%s%N | md5sum | cut -c 1-8)
-
-    green "正在配置 Tuic..."
-    mkdir /etc/tuic >/dev/null 2>&1
-    cat << EOF > /etc/tuic/tuic.json
-{
-    "port": $port,
-    "token": ["$token"],
-    "certificate": "$cert_path",
-    "private_key": "$key_path",
-    "ip": "::",
-    "congestion_controller": "bbr",
-    "alpn": ["h3"]
-}
 EOF
-    mkdir /root/tuic >/dev/null 2>&1
-    cat << EOF > /root/tuic/tuic-client.json
-{
-    "relay": {
-        "server": "$domain",
-        "port": $port,
-        "token": "$token",
-        "ip": "$ip",
-        "congestion_controller": "bbr",
-        "udp_relay_mode": "quic",
-        "alpn": ["h3"],
-        "disable_sni": false,
-        "reduce_rtt": false,
-        "max_udp_relay_packet_size": 1500
-    },
-    "local": {
-        "port": 6080,
-        "ip": "127.0.0.1"
-    },
-    "log_level": "off"
-}
-EOF
-    cat << EOF > /root/tuic/tuic.txt
-Sagernet 与 小火箭 配置说明（以下6项必填）：
-{
-    服务器地址：$domain
-    服务器端口：$port
-    token：$token
-    ALPN：h3
-    UDP 转发：开启
-    UDP 转发模式：QUIC
-    拥塞控制：bbr
-}
-EOF
-    cat << EOF > /root/tuic/clash-meta.yaml
-mixed-port: 7890
-external-controller: 127.0.0.1:9090
-allow-lan: false
-mode: rule
-log-level: debug
-ipv6: true
-dns:
-  enable: true
-  listen: 0.0.0.0:53
-  enhanced-mode: fake-ip
-  nameserver:
-    - 8.8.8.8
-    - 1.1.1.1
-    - 114.114.114.114
-
-proxies:
-  - name: Misaka-tuicV4
-    server: $domain
-    port: $port
-    type: tuic
-    token: $token
-    ip: $ip
-    alpn: [h3]
-    disable-sni: true
-    reduce-rtt: true
-    request-timeout: 8000
-    udp-relay-mode: quic
-    congestion-controller: bbr
-
-proxy-groups:
-  - name: Proxy
-    type: select
-    proxies:
-      - Misaka-tuicV4
-      
-rules:
-  - GEOIP,CN,DIRECT
-  - MATCH,Proxy
-EOF
-    
-    url="tuic://$domain:$port?password=$token&alpn=h3&mode=bbr#tuic-misaka"
-    echo ${url} > /root/tuic/url.txt
-
-    cat << EOF >/etc/systemd/system/tuic.service
-[Unit]
-Description=tuic Service
-Documentation=https://gitlab.com/Misaka-blog/tuic-script
-After=network.target
-[Service]
-User=root
-ExecStart=/usr/local/bin/tuic -c /etc/tuic/tuic.json
-Restart=on-failure
-RestartSec=10
-LimitNOFILE=infinity
-[Install]
-WantedBy=multi-user.target
-EOF
-    
-    systemctl daemon-reload
-    systemctl enable tuic
-    systemctl start tuic
-
-    if [[ -n $(systemctl status tuic 2>/dev/null | grep -w active) && -f '/etc/tuic/tuic.json' ]]; then
-        green "tuic 服务启动成功"
-    else
-        red "tuic 服务启动失败，请运行 systemctl status tuic 查看服务状态并反馈，脚本退出" && exit 1
-    fi
-
-    showconf
-}
-
-unst_tuv4(){
-    systemctl stop tuic
-    systemctl disable tuic
-    rm -f /etc/systemd/system/tuic.service /root/tuic.sh
-    rm -rf /usr/local/bin/tuic /etc/tuic /root/tuic
-    
-    green "Tuic V4 已彻底卸载完成！"
-}
 
 inst_tuv5(){
     if [[ $(tuic -v) == "0.8.5" ]]; then
@@ -519,7 +370,7 @@ tuicswitch(){
 }
 
 changeport(){
-    if [[ $(tuic -v) == "0.8.5" ]]; then
+    if [[ $(tuic -v) == "1.0.0" ]]; then
         oldport=$(cat /etc/tuic/tuic.json 2>/dev/null | sed -n 2p | awk '{print $2}'| tr -d ',')
 
         read -p "设置 tuic 端口 [1-65535]（回车则随机分配端口）：" port
@@ -627,18 +478,7 @@ changepasswd(){
 }
 
 changeconf(){
-    if [[ $(tuic -v) == "0.8.5" ]]; then
-        green "Tuic V4 配置变更选择如下:"
-        echo -e " ${GREEN}1.${PLAIN} 修改端口"
-        echo -e " ${GREEN}2.${PLAIN} 修改Token"
-        echo ""
-        read -p " 请选择操作[1-2]：" confAnswer
-        case $confAnswer in
-            1 ) changeport ;;
-            2 ) changetoken ;;
-            * ) exit 1 ;;
-        esac
-    else
+    if [[ $(tuic -v) == "1.0.0" ]]; then
         green "Tuic V5 配置变更选择如下:"
         echo -e " ${GREEN}1.${PLAIN} 修改端口"
         echo -e " ${GREEN}2.${PLAIN} 修改 UUID"
