e2183698a596902eddf64f648881da04340b40d2
xxf185 <109097973+xxf185@users.noreply.github.com>
2026-06-25 00:39:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,10 +69,10 @@ detect_arch() {
     local arch
     arch=$(uname -m)
     case $arch in
-        x86_64)  echo "x86_64-linux-musl";;
-        i686)    echo "i686-linux-musl";;
-        aarch64) echo "aarch64-linux-musl";;
-        armv7l)  echo "armv7-linux-musl";;
+        x86_64)  echo "x86_64-linux";;
+        i686)    echo "i686-linux";;
+        aarch64) echo "aarch64-linux";;
+        armv7l)  echo "armv7-linux";;
         *)       error "不支持的系统架构: $arch";;
     esac
 }
